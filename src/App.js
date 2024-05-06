12bed436f69f87f0b52ac10c5811bd8c2051481d
murthasaali <murthasaalick123@gmail.com>
2024-05-06 10:21:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import InvoicePage from './pages/InvoicePage';
 import './App.css';
+import invoiceData from './Constants/Datas/InvoiceDatas';
 
 function App() {
+  useEffect(() => {
+    // Store invoice data in localStorage when the component mounts
+    localStorage.setItem('invoiceData', JSON.stringify(invoiceData));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/invoice" element={<InvoicePage />} />
+      </Routes>
+    </Router>
   );
 }
 
